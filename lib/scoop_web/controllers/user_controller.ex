d750ac0649af91525e8795ad8d5e0f5673d0feb1
Joe Banks <joseph@josephbanks.me>
2020-10-15 16:18:34 +0100

    Add user controller for creating and showing users

---
+++ lib/scoop_web/controllers/user_controller.ex
@@ -0,0 +1,36 @@
+defmodule ScoopWeb.UserController do
+  use ScoopWeb, :controller
+
+  alias Scoop.{Repo, User}
+
+  def index(conn, _params) do
+    json conn, %{
+      "status" => "okay",
+      "message" => "Index for the user controller"
+    }
+  end
+
+  def create(conn, params) do
+    changeset = User.changeset(%User{}, params)
+
+    case Repo.insert(changeset) do
+      {:ok, _} -> json conn, %{status: "okay"}
+      {:error, changeset} -> 
+        conn
+        |> put_status(400)
+        |> json(%{errors: Scoop.Utils.changeset_error_to_string(changeset)})
+    end
+  end
+
+  def show(conn, %{"id" => user_id}) do
+    user = Repo.get(User, user_id)
+
+    case user do
+      nil ->
+        conn
+        |> put_status(404)
+        |> json %{errors: %{detail: "User not found"}}
+      user -> json conn, Scoop.Utils.model_to_map(user, [:email, :full_name])
+    end
+  end
+end

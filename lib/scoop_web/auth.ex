b79fe77d0f0e3dfcda71c557da0e085640645602
Joe Banks <joseph@josephbanks.me>
2020-11-17 14:39:22 +0000

    Fix queries from unauthenticated user

--- lib/scoop_web/auth.ex
+++ lib/scoop_web/auth.ex
@@ -6,24 +6,32 @@ defmodule ScoopWeb.Plugs.SetCurrentUser do
   def init(_params) do
   end
 
+  def assign_nil_user(conn) do
+      assign(conn, :current_user, nil)
+      assign(conn, :signed_in?, false)
+  end
+
+  def assign_logged_in_user(conn, user) do
+    assign(conn, :current_user, user)
+    assign(conn, :signed_in?, true)
+  end
+
   def call(conn, _params) do
     token = get_req_header(conn, "authorization")
 
-    token = if (token |> length) > 0 do
-      token |> Enum.at(0)
+    if (token |> length) > 0 do
+      token = Enum.at(0)
+
+      user = Repo.get_by(User, token: token)
+
+      case user do
+        nil ->
+          conn |> assign_nil_user
+        user ->
+          conn |> assign_logged_in_user(user)
+      end
     else
-      nil
-    end
-
-    user = Repo.get_by(User, token: token)
-
-    case user do
-      nil ->
-        assign(conn, :current_user, nil)
-        assign(conn, :signed_in?, false)
-      user ->
-        assign(conn, :current_user, user)
-        assign(conn, :signed_in?, true)
+      conn |> assign_nil_user
     end
   end
 end

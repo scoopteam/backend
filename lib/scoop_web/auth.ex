3dcf5de0802ab1827fbaf4662877fdf1d0df2508
Joe Banks <joseph@josephbanks.me>
2020-11-17 14:33:08 +0000

    Fix auth plug

--- lib/scoop_web/auth.ex
+++ lib/scoop_web/auth.ex
@@ -7,7 +7,13 @@ defmodule ScoopWeb.Plugs.SetCurrentUser do
   end
 
   def call(conn, _params) do
-    token = get_req_header(conn, "Authorization")
+    token = get_req_header(conn, "authorization")
+
+    token = if (token |> length) > 0 do
+      token |> Enum.at(0)
+    else
+      nil
+    end
 
     user = Repo.get_by(User, token: token)
 

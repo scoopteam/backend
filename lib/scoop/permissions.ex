4a67bdb7976c7ee3ed4881ae2db2d0d82e6f5fa1
Joe Banks <joseph@josephbanks.me>
2020-11-24 09:28:20 +0000

    Add permissions utils

---
+++ lib/scoop/permissions.ex
@@ -0,0 +1,11 @@
+defmodule Scoop.Permissions do
+  def has_perm?(perms, perm) do
+    perm in perms
+  end
+
+  def has_any_perm?(user_perms, requested_perms) do
+    requested_perms
+    |> Enum.map(&(&1 in user_perms))
+    |> Enum.any?
+  end
+end

86af3dd083738724e4d9cde98c538d020863ac24
Joe Banks <joseph@josephbanks.me>
2021-01-14 18:53:51 +0000

    Redeploy

--- mix.exs
+++ mix.exs
@@ -14,9 +14,6 @@ defmodule Scoop.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Scoop.Application, []},

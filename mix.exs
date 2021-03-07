c9efa958f5f536870abd722e2c55a1fb907acff2
Joe Banks <joseph@josephbanks.me>
2021-03-07 18:46:37 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -21,7 +21,6 @@ defmodule Scoop.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
